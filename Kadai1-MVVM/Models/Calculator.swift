913cb57471904d7f75c64dbdff1ba8b5bf51db0e
mikekondo <foad5969@gmail.com>
2022-10-01 21:34:20 +0900

    update: Model修正

--- Kadai1-MVVM/Models/Calculator.swift
+++ Kadai1-MVVM/Models/Calculator.swift
@@ -8,9 +8,6 @@
 import Foundation
 
 class Calculator {
-    private var number1 = 0
-    private var number2 = 0
-    private var number3 = 0
     var sum = 0
 
     func addition(number1: Int,number2: Int,number3: Int) {
